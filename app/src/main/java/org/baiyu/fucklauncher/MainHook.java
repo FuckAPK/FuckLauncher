53394b05e40192803dda8c90290ccd4165428fbc
lyaaz <45838887+lyaaz@users.noreply.github.com>
2023-10-04 22:17:12 +0800

    init

---
+++ app/src/main/java/org/baiyu/fucklauncher/MainHook.java
@@ -0,0 +1,45 @@
+package org.baiyu.fucklauncher;
+
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+import com.topjohnwu.superuser.Shell;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class MainHook implements IXposedHookLoadPackage {
+
+    private static final ArrayList<String> PACKAGE_NAMES = new ArrayList<>(Arrays.asList(
+            "com.google.android.apps.nexuslauncher",
+            "com.android.launcher3"
+    ));
+    private static final String CLASS_NAME = "com.android.launcher3.touch.WorkspaceTouchListener";
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (! PACKAGE_NAMES.contains(lpparam.packageName)) {
+            return;
+        }
+
+        XposedBridge.hookMethod(
+                GestureDetector.SimpleOnGestureListener.class.getMethod("onDoubleTap", MotionEvent.class),
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        if (!param.thisObject.getClass().getName().equals(CLASS_NAME)) {
+                            return;
+                        }
+                        XposedBridge.log("double tap detected " + param.thisObject.getClass().getName());
+                        Shell.cmd("input keyevent 223").exec();
+                        param.setResult(true);
+                    }
+                }
+        );
+    }
+}
