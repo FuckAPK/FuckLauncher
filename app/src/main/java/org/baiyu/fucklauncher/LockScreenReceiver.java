1f86f6a565880e8d13fea72d4a60fb2d68b23625
lyaaz <45838887+lyaaz@users.noreply.github.com>
2023-10-06 09:06:10 +0800

    move root to module part

---
+++ app/src/main/java/org/baiyu/fucklauncher/LockScreenReceiver.java
@@ -0,0 +1,57 @@
+package org.baiyu.fucklauncher;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.topjohnwu.superuser.Shell;
+
+import java.util.UUID;
+
+public class LockScreenReceiver extends BroadcastReceiver {
+
+    private static final String PREF_KEY = "key";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        SharedPreferences prefs = getPrefs(context);
+
+        String key = prefs.getString(PREF_KEY, null);
+        if (key == null) {
+            key = generateKey(prefs);
+        }
+
+        if (key.equals(intent.getStringExtra(PREF_KEY))) {
+            lockScreen();
+        }
+    }
+
+    private void lockScreen() {
+        Shell.cmd("input keyevent 223").exec();
+    }
+
+    /** @noinspection deprecation*/
+    @SuppressLint("WorldReadableFiles")
+    private SharedPreferences getPrefs(Context context) {
+        SharedPreferences prefs = null;
+        try {
+            prefs = context.getSharedPreferences(BuildConfig.APPLICATION_ID + "_preferences", Context.MODE_WORLD_READABLE);
+        } catch (SecurityException e) {
+            Log.e("fucklauncher", e.toString());
+        }
+        assert prefs != null;
+        return prefs;
+    }
+
+    private String generateKey(SharedPreferences prefs) {
+        SharedPreferences.Editor editor = prefs.edit();
+        String key = UUID.randomUUID().toString();
+        editor.putString(PREF_KEY, key);
+        editor.apply();
+        return key;
+    }
+}
--- app/src/main/java/org/baiyu/fucklauncher/MainHook.java
+++ app/src/main/java/org/baiyu/fucklauncher/MainHook.java
@@ -1,15 +1,18 @@
 package org.baiyu.fucklauncher;
 
+import android.app.AndroidAppHelper;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
-
-import com.topjohnwu.superuser.Shell;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
@@ -20,6 +23,7 @@ public class MainHook implements IXposedHookLoadPackage {
             "com.android.launcher3"
     ));
     private static final String CLASS_NAME = "com.android.launcher3.touch.WorkspaceTouchListener";
+    private static final XSharedPreferences prefs = new XSharedPreferences(BuildConfig.APPLICATION_ID);
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
@@ -35,7 +39,15 @@ public class MainHook implements IXposedHookLoadPackage {
                         if (!param.thisObject.getClass().getName().equals(CLASS_NAME)) {
                             return;
                         }
-                        Shell.cmd("input keyevent 223").exec();
+                        Context mContext = AndroidAppHelper.currentApplication();
+
+                        String key = prefs.getString("key", null);
+
+                        Intent intent = new Intent("org.baiyu.fucklauncher.LockScreen")
+                                .setComponent(new ComponentName(BuildConfig.APPLICATION_ID, LockScreenReceiver.class.getName()))
+                                .putExtra("key", key);
+                        mContext.sendBroadcast(intent);
+
                         param.setResult(true);
                     }
                 }
