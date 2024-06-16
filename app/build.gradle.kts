ddd70338757ba0c481a8f254efe3cd18c529ca15
lyaaz <45838887+lyaaz@users.noreply.github.com>
2024-06-16 22:11:54 +0800

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.baiyu.fucklauncher"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "6.0"
+        versionCode = 10
+        versionName = "6.1"
 
         resourceConfigurations += setOf("en")
 
